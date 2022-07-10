b29f56fe4bbf09997366649dce7c8fe701300505
MD DANISH JAMAL <mddanishjamal04@yahoo.com>
2022-07-10 08:35:02 +0530

    created sign up end point

---
+++ backend/routes/auth.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const User=require('../models/User');
+const router=express.Router();
+const { body, validationResult } = require('express-validator');
+
+//create a user using:Post "/api/auth": ..Does not require auth
+router.post('/',[
+
+  
+  body('name',"Enter a valid name").isLength({min:3}),
+  body('email',"Enter a valid email").isEmail(),
+  body('password',"Password must be atleast 5 character").isLength({ min: 5 }),
+
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email:req.body.email,
+      }).then(user => res.json(user)).catch(err=>{
+        res.json({error:"Please Enter a unique value for email",message:err.message})
+      })
+      
+})
+
+
+module.exports=router;
